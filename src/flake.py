4cf3a5bdd823134d52f665cc9163c9b5333fde24
Darrin Mossor <github@mossor.org>
2023-12-29 19:25:51 -0800

    Remove all reference to ball/Ball/balls, migrate to more generic thing/Thing/things.  Code is working same as basic Balls code but with flake.png

---
+++ src/flake.py
@@ -0,0 +1,97 @@
+import pyglet
+from files import thing, load
+from collections import namedtuple
+
+# Set up a window
+myWindow = namedtuple('myWindow', ['X', 'Y'])
+myWin = myWindow(800, 600)
+game_window = pyglet.window.Window(myWin.X, myWin.Y)
+
+main_batch = pyglet.graphics.Batch()
+
+# Set up the two top labels
+#score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
+level_label = pyglet.text.Label(text="Testing - Flakes!", x=400, y=575, anchor_x='center', batch=main_batch)
+
+# Set up the game over label offscreen
+game_over_label = pyglet.text.Label(text="GAME OVER",
+                                    x=400, y=-300, anchor_x='center',
+                                    batch=main_batch, font_size=48)
+
+counter = pyglet.window.FPSDisplay(window=game_window)
+
+game_objects = []
+
+# We need to pop off as many event stack frames as we pushed on
+# every time we reset the level.
+event_stack_size = 0
+
+def init():
+    global num_things
+
+    num_things = 4
+    reset_level()
+
+
+def reset_level():
+    global game_objects, event_stack_size
+
+    # Clear the event stack of any remaining handlers from other levels
+    while event_stack_size > 0:
+        game_window.pop_handlers()
+        event_stack_size -= 1
+
+    things = load.things(num_things, myWin, main_batch)
+
+    # Store all objects that update each frame in a list
+    game_objects = things
+
+    # Add any specified event handlers to the event handler stack
+    for obj in game_objects:
+        for handler in obj.event_handlers:
+            game_window.push_handlers(handler)
+            event_stack_size += 1
+
+
+@game_window.event
+def on_draw():
+    game_window.clear()
+    main_batch.draw()
+    counter.draw()
+
+def update(dt):
+    global num_things
+
+    # To avoid handling collisions twice, we employ nested loops of ranges.
+    # This method also avoids the problem of colliding an object with itself.
+    for i in range(len(game_objects)):
+        for j in range(i + 1, len(game_objects)):
+
+            obj_1 = game_objects[i]
+            obj_2 = game_objects[j]
+
+            # Make sure the objects haven't already been killed
+            if not obj_1.dead and not obj_2.dead:
+                if obj_1.collides_with(obj_2):
+                    obj_1.handle_collision_with(obj_2)
+                    print('DLM: Obj1 collides with Obj2')
+
+
+    # Let's not modify the list while traversing it
+    to_add = []
+
+    for obj in game_objects:
+        obj.update(dt)
+
+        to_add.extend(obj.new_objects)
+        obj.new_objects = []
+
+if __name__ == "__main__":
+    # Start it up!
+    init()
+
+    # Update the game 120 times per second
+    pyglet.clock.schedule_interval(update, 1 / 120.0)
+
+    # Tell pyglet to do its thing
+    pyglet.app.run()
